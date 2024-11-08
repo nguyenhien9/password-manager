8fb9020e7b11383817235ba18352eeca38671f6b
NguyenHien <ngnghien5588@gmail.com>
2024-11-08 21:40:57 +0700

    save local

--- server/src/main.ts
+++ server/src/main.ts
@@ -13,6 +13,7 @@ async function bootstrap() {
 
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, documentFactory);
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
+  console.log('Application is running on: ' + (await app.getUrl()));
 }
 bootstrap();
